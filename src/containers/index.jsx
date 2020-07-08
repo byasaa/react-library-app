e7644b0dd8cdb1613d2b95cc115b30199cf5319c
byasaa <abi.vardhana@outlook.com>
2020-07-08 16:02:21 +0700

    add author, history, genre

--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -2,10 +2,16 @@ import Sidebar from "./Sidebar"
 import Navbar from "./Navbar"
 import Modals from "../components/Modal"
 import Content from "./Content"
+import GenresTable from '../components/GenresTable'
+import AuthorsTable from '../components/AuthorsTable'
+import HistoryTable from '../components/HistoryTable';
 
 export {
     Sidebar,
     Navbar,
     Modals,
-    Content
+    Content,
+    AuthorsTable,
+    GenresTable,
+    HistoryTable
 }
