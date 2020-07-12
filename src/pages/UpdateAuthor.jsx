d23fc412dac59caaa44126da4fb8ebaeae81f976
byasaa <abi.vardhana@outlook.com>
2020-07-13 06:13:59 +0700

    update page author, genre

---
+++ src/pages/UpdateAuthor.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import {
+  Sidebar,
+  Navbar,
+} from '../containers/index'
+import {Container, Col, Row} from 'reactstrap'
+import "../styles/layout.css"
+import UpdateForm from "../components/author/UpdateForm";
+
+
+class UpdateAuthor extends Component {
+
+    render() {
+        return (
+            <>
+              <Container fluid>
+                <Row>
+                <Col md={3} id="sidebar-wrapper" style={{ marginRight: '-15px' }}>
+                  <Sidebar {...this.props} />
+                </Col>
+                <Col md={9} id="page-content-wrapper">
+                    <Navbar {...this.props} />
+                    <div className="body">
+                      <UpdateForm {...this.props} />
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </>
+          )
+    }
+}
+
+export default UpdateAuthor
